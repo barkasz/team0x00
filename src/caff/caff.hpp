84ca15eb052c213f7dc110747c6e02c9a72f9c7b
Ákos Boros <boros.aki@gmail.com>
2021-11-08 22:03:11 +0100

    CAFF - [feature] - caff_t type created

--- src/caff/caff.hpp
+++ src/caff/caff.hpp
@@ -8,13 +8,13 @@
 #include <stdint.h>
 #include <vector>
 
-struct caff_header {
+struct caff_header_t {
 	uint8_t magic[4];
 	uint64_t header_size;
 	uint64_t num_anim;
 };
 
-struct caff_credits {
+struct caff_credits_t {
 	uint16_t year;
 	uint8_t month;
 	uint8_t day;
@@ -24,15 +24,22 @@ struct caff_credits {
 	std::vector<uint8_t> creator;
 };
 
-struct caff_animation {
+struct caff_animation_t {
 	uint64_t duration;
 	uint8_t ciff; 		// TODO: replace with CIFF type
 };
 
-struct block {
+struct block_t {
 	uint8_t id;
 	uint64_t length;
 	std::vector<uint8_t> data;
 };
+
+struct caff_t {
+	caff_header_t header;
+	std::vector<caff_credits_t> credits;
+	std::vector<caff_animation_t> animation;
+};
+
 
 #endif //TEAM0X00_INCLUDE_CAFF_CAFF_HPP
