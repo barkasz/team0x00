00d2a141cbd40d15cbbd779730cedb7fcfc19315
BalintBoredAF <ferenczybalint@edu.bme.hu>
2021-11-23 22:41:41 +0100

    Explore page created

---
+++ frontend/team0x00-app/src/components/Content/Content.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react'
+import './content.css'
+import { Route, Link } from 'react-router-dom'
+import searchIcon from '../../assets/search.svg'
+import menuIcon from '../../assets/menu.svg'
+import Post from '../Post/Post'
+
+import _posts from '../../data/posts'
+
+class Content extends Component {
+    render(){
+        const {currentUser, ...props} = this.props
+        const posts = _posts
+        
+        return (
+                <div className="content">
+                    <div className="wrapper">
+
+                            <div className="header">
+                                <img className='menuBtn' src={menuIcon} alt="" onClick={() => props.toggleMenu()}/>
+                                <div className="input-with-icon">
+                                    <img src={searchIcon} alt="Search icon" className="icon search-icon"/>
+                                    <input type="text" placeholder="Search..."/>
+                                </div>
+                                <Link to='/upload' className='btn btn-primary mb-0'>Upload</Link>
+                            </div>
+
+                        <Route exact path = "/" render={() => (
+                            <>
+                                <h1 className='mb-4'>Explore</h1>
+                                { posts.map(post => (
+                                    <Post key={post.id} post={post} currentUser={currentUser}/>
+                                ))
+                                }
+                                
+                            </>
+                        )}/>
+                    </div>
+                </div>
+        )
+    }
+}
+
+export default Content
--- frontend/team0x00-app/src/data/posts.js
+++ frontend/team0x00-app/src/data/posts.js
@@ -1,16 +1,23 @@
 
-const posts = [{
-    id: 0,
-    description: "CAFF 1",
-    imageLink: "https://cdn2.scratch.mit.edu/get_image/gallery/27711989_170x100.png"
-  }, {
-    id: 1,
-    description: "CAFF 2",
-    imageLink: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/5d1cd673416639.5c0899673a27e.gif"
-  }, {
-    id: 2,
-    description: "CAFF 3",
-    imageLink: "https://c.tenor.com/ANpZrm1voMEAAAAd/quokka-animals.gif"
-  }]
+import tea from '../assets/tea.webp'
+import home from '../assets/architecture.webp'
+import users from './users'
 
-  export default posts
+const posts = [
+    {
+      id: 0,
+      title: 'Having a nice cup with bae ❤️',
+      image: tea,
+      user: users[0],
+      comment_ids: [ 0, 1, 2 ]
+    },
+    {
+      id: 1,
+      title: 'Freshly designed (still not paid tho)',
+      image: home,
+      user: users[3],
+      comment_ids: [ 3, 4 ]
+    },
+  ]
+
+export default posts
