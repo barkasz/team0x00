edf2658e4748f62b49076e2867316297cbdc3c07
BalintBoredAF <ferenczybalint@edu.bme.hu>
2021-11-25 12:24:42 +0100

    Popups added

--- frontend/team0x00-app/src/components/Post/Post.js
+++ frontend/team0x00-app/src/components/Post/Post.js
@@ -5,10 +5,49 @@ import pic from '../../assets/tea.webp'
 import commentIcon from '../../assets/comment.svg'
 import editIcon from '../../assets/edit.svg'
 import deleteIcon from '../../assets/trash.svg'
-
+import Popup from '../Popup/Popup'
 import _comments from '../../data/comments'
 
 class Post extends Component {
+
+    constructor(){
+        super()
+        this.handleDeletePostPopup = this.handleDeletePostPopup.bind(this)
+        this.handleDeleteUserPopup = this.handleDeleteUserPopup.bind(this)
+        this.toggleDeleteUserPopup = this.toggleDeleteUserPopup.bind(this)
+        this.toggleDeletePostPopup = this.toggleDeletePostPopup.bind(this)
+        this.state = {
+            deletePostPopup: false,
+            deleteUserPopup: false
+        }
+    }
+
+    handleDeletePostPopup(resp){
+        if(resp) {
+            // TODO delete user
+        }
+        this.toggleDeletePostPopup()
+    }
+
+    handleDeleteUserPopup(resp){
+        if(resp) {
+            // TODO delete user
+        }
+        this.toggleDeleteUserPopup()
+    }
+
+    toggleDeletePostPopup(){
+        this.setState({
+            deletePostPopup: !this.state.deletePostPopup
+        })
+    }
+
+    toggleDeleteUserPopup(){
+        this.setState({
+            deleteUserPopup: !this.state.deleteUserPopup
+        })
+    }
+
     getComments(idList){
         return _comments.filter(comment => idList.includes(comment.id))
     }
@@ -16,15 +55,33 @@ class Post extends Component {
     render() {
         const { post, currentUser } = this.props 
         const comments = this.getComments(post.comment_ids)
-        return ( <div className='post'>
+        return ( <>
+        {this.state.deleteUserPopup && 
+        
+        <Popup title={"Deleting User"} 
+               text={"Are you sure you want to delete the account of " + (post?.user.username || "unknown") + "?"} 
+               handleClose={this.handleDeleteUserPopup}
+        /> 
+        }
+
+        {this.state.deletePostPopup && 
+        
+        <Popup title={"Deleting Post"} 
+               text={"Are you sure you want to delete this post \"" + (post?.title || "unknown post title") +"\"?"} 
+               handleClose={this.handleDeletePostPopup}
+        /> 
+        }
+
+        <div className='post'>
             {currentUser?.admin &&
-                <div className="action-button delete-post">
+                <div className="action-button delete-post" onClick={this.toggleDeletePostPopup}>
                     <img src={deleteIcon} className='icon' alt="" />
                 </div>
             }
 
             <img src={post?.image || pic} className="post-img" alt={post?.title || 'Posted pic'}/>
             <div className="post-info">
+                
                 <h2>{post?.title || 'No title'}</h2>
                 <div className="author-bar mb-3">
                     <div className="profile profile-medium">
@@ -39,8 +96,8 @@ class Post extends Component {
                         <div className="action-button">
                             <img src={editIcon} alt="" className='icon' />
                         </div>
-                        <div className="action-button">
-                            <img src={deleteIcon} alt="" className='icon' />
+                        <div className="action-button" onClick={this.toggleDeleteUserPopup}>
+                            <img src={deleteIcon} alt="" className='icon'/>
                         </div>
                     </div>
                     }
@@ -62,6 +119,7 @@ class Post extends Component {
                 </div>
             </div>
         </div>
+        </>
         )
     }
 
