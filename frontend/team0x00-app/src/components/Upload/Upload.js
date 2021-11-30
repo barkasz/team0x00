8884c25298327ff053a6bd39b2ca491a476da8eb
BalintBoredAF <ferenczybalint@edu.bme.hu>
2021-11-30 19:31:58 +0100

    Upload form fixed

--- frontend/team0x00-app/src/components/Upload/Upload.js
+++ frontend/team0x00-app/src/components/Upload/Upload.js
@@ -14,11 +14,18 @@ class Upload extends Component {
     onImageChange = event => {
         if (event.target.files && event.target.files[0]) {
           let img = event.target.files[0];
+          console.log(img)
           this.setState({
-            image: URL.createObjectURL(img)
+            image: img
           });
         }
       };
+    
+    sizeOf = function (bytes) {
+        if (bytes == 0) { return "0.00 B"; }
+        var e = Math.floor(Math.log(bytes) / Math.log(1024));
+        return (bytes/Math.pow(1024, e)).toFixed(2)+' '+' KMGTP'.charAt(e)+'B';
+    }
 
     render(){
         const file = React.createRef();
@@ -28,10 +35,19 @@ class Upload extends Component {
         return  (
             <form action="">
             <div className="upload">
-                    <div onClick={clickForwarder} className={`selector ${!this.state.image ? "bordered" : ""}`}>
-                        {this.state.image && <img src={this.state.image} alt="" />}
-                        <input type="file" id='file' onChange={this.onImageChange} ref={file} style={{display: 'none'}}/>
-                        {!this.state.image && <h3 className='subtle'>Click to select image</h3>}
+                    <div className="warning">
+                        <p>This is an example warning message.</p>
+                    </div>
+                    <div onClick={clickForwarder} className='selector bordered'>
+                        <input type="file" accept='.caff' id='file' onChange={this.onImageChange} ref={file} style={{display: 'none'}}/>
+                        <h3 className='subtle'>Click to select caff file</h3>
+                        {this.state.image &&
+                            <div className="file">
+                                <h3>{this.state.image.name}</h3>
+                                <p>{this.sizeOf(this.state.image.size)}</p>
+                            </div>
+                        }
+                        
                     </div>
                     <div className="upload-form-elements">
                         <h3 className='mt-0 mb-2'>Set image title</h3>
