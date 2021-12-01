c6b76c789f5b9c3d6f099b97f686387fd727ea26
BalintBoredAF <ferenczybalint@edu.bme.hu>
2021-12-01 17:12:00 +0100

    Post turned into function component

--- frontend/team0x00-app/src/components/Post/Post.js
+++ frontend/team0x00-app/src/components/Post/Post.js
@@ -1,77 +1,41 @@
-import React, {Component} from 'react'
+import React, {useState} from 'react'
 import './post.css'
 import Comment from '../Comment/Comment'
 import pic from '../../assets/tea.webp'
 import commentIcon from '../../assets/comment.svg'
-import editIcon from '../../assets/edit.svg'
 import deleteIcon from '../../assets/trash.svg'
 import downloadIcon from '../../assets/download.svg'
 import Popup from '../Popup/Popup'
 import _comments from '../../data/comments'
-import { Link } from 'react-router-dom'
 import { API } from '../../api-service'
 import randomProfilePic from '../../data/profile_pic'
 import defaultProfilePic from '../../assets/default-user.png'
+import useToken from '../../hooks/useToken'
 
-class Post extends Component {
-    constructor(props){
-        super(props)
+function Post ({post}) {
+    const [deletePopup, setDeletePopup] = useState(false)
+    const [comments, setComments] = useState(post.comments)
+    const [profilePic] = useState(randomProfilePic())
+    const [newComment, setNewComment] = useState()
+    const { token } = useToken()
 
-        this.state = {
-            deletePostPopup: false,
-            post: props.post,
-            profilePic: randomProfilePic(),
-            comment: '',
-            comment_list : [],
-            _comments : []
-        }
-    }
-
-    handleDeletePostPopup = (resp) => {
+    const handleDeletePopup = (resp) => {
         if(resp) {
-            API.deletePost(this.state.post)
+            API.deletePost(post)
         }
-        this.toggleDeletePostPopup()
+        toggleDeletePopup()
     }
 
-    toggleDeletePostPopup = () => {
-        this.setState({
-            deletePostPopup: !this.state.deletePostPopup
-        })
+    const toggleDeletePopup = () => {
+       setDeletePopup(!deletePopup)
     }
 
-    getComments = (idList)  => {
-        return _comments.filter(comment => idList.includes(comment.id))
-    }
-
-    requestData = async () => {
-        await API.getComments().then(data => {
-            // console.log(data);
-            const data2 = data.filter(comment => this.props.post.comment_ids.includes(comment.id))
-            // console.log(data2);
-            this.setState({comment_list : data2})
-        })
-        .catch(error => {
-            console.error('There was an error in requestData!', error);
-        });        
-    }
-
-    componentDidMount(){
-        this.requestData()
-
-    }
-
-
-
-    render() {
-        const { post, currentUser } = this.props 
-        
         return ( <>
-        {this.state.deletePostPopup && 
+        {deletePopup && 
         
         <Popup title={"Deleting Post"} 
                text={"Are you sure you want to delete this post \"" + (post?.title || "unknown post title") +"\"?"} 
-               handleClose={this.handleDeletePostPopup}
+               handleClose={handleDeletePopup}
         /> 
         }
 
@@ -82,8 +46,8 @@ class Post extends Component {
                     <img src={downloadIcon} className='icon' alt="Download icon" />
                 </div>
             
-            {!currentUser?.admin &&
-                <div className="action-button" onClick={() => this.toggleDeletePostPopup() }>
+            {!token?.admin &&
+                <div className="action-button" onClick={() => toggleDeletePopup() }>
                     <img src={deleteIcon} className='icon' alt="Delete icon" />
                 </div>
             }
@@ -96,7 +60,7 @@ class Post extends Component {
                 <h2>{post?.title || 'No title'}</h2>
                 <div className="author-bar mb-3">
                     <div className="profile profile-medium">
-                        <img src={this.state.profilePic} onError={(e)=>{e.target.onerror = null; e.target.src=defaultProfilePic}} alt="Poster's profile pic"/> 
+                        <img src={profilePic} onError={(e)=>{e.target.onerror = null; e.target.src=defaultProfilePic}} alt="Poster's profile pic"/> 
                     </div>
                     <div className="info">
                         <p>{post?.author || 'Unknown'}</p>
@@ -105,23 +69,22 @@ class Post extends Component {
                 <div className="comment-section">
                     <div className="comments">
                         {
-                            this.state.comment_list?.map(comment => (<Comment key={comment.id} comment={comment}/>))
+                            comments.map(comment => (<Comment key={comment.id} comment={comment}/>))
                         }
                     </div>
                     
                     <div className="write w-100">
                         <div className="input-with-icon w-100">
                             <img src={commentIcon} alt="Comment icon" className="icon"/>
-                            <input type="text" className='w-100' placeholder="Write a comment here..." value={this.state.comment} onChange={ (event) => this.setState({comment: event.target.value})} />
+                            <input type="text" className='w-100' placeholder="Write a comment here..." value={newComment} onChange={ (event) => setNewComment(event.target.value)} />
                         </div>
-                        <button className='btn btn-small' onClick={() => alert(this.state.comment)}>Send</button>
+                        <button className='btn btn-small' onClick={() => setComments([...comments, newComment])}>Send</button>
                     </div>
                 </div>
             </div>
         </div>
         </>
         )
-    }
 
 }
 
